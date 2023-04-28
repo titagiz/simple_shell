5a6fbda7b3fe0015d1f19d2908807be0dbefd0ac
titagiz <yenealemgizachew123@gmail.com>
2023-04-28 12:13:47 -0700

    tasks

--- shell.h
+++ shell.h
@@ -1,67 +1,24 @@
-#ifndef SHELL_H
-#define SHELL_H
-
+#ifndef SIMPLE_SHELL
+#define SIMPLE_SHELL
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
-#include <sys/wait.h>
 #include <sys/types.h>
-#include <errno.h>
-#include <stddef.h>
 #include <sys/stat.h>
+#include <sys/wait.h>
+#include <limits.h>
 #include <signal.h>
-
-int _putchar(char c);
+char *read_line(void);
+char **splits(char *line, char *delim);
+int execute(char **parse);
 void _puts(char *str);
-int _strlen(char *s);
+int _putchar(char c);
+unsigned int _strlen(char *s);
+char **find_path(char **environ);
+char *_getenv(char **environ, char *dirname);
+char *arg_path(char **parse, char **tokens);
 char *_strdup(char *str);
-char *concat_all(char *name, char *sep, char *value);
-
-char **splitstring(char *str, const char *delim);
-void execute(char **argv);
-void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
-
-
+char *if_exists(char **environ);
 extern char **environ;
-
-/**
- * struct list_path - Linked list containing PATH directories
- * @dir: directory in path
- * @p: pointer to next node
- */
-typedef struct list_path
-{
-	char *dir;
-	struct list_path *p;
-} list_path;
-
-
-char *_getenv(const char *name);
-list_path *add_node_end(list_path **head, char *str);
-list_path *linkpath(char *path);
-char *_which(char *filename, list_path *head);
-
-/**
- * struct mybuild - pointer to function with corresponding buildin command
- * @name: buildin command
- * @func: execute the buildin command
- */
-typedef struct mybuild
-{
-	char *name;
-	void (*func)(char **);
-} mybuild;
-
-void(*checkbuild(char **arv))(char **arv);
-int _atoi(char *s);
-void exitt(char **arv);
-void env(char **arv);
-void _setenv(char **arv);
-void _unsetenv(char **arv);
-
-void freearv(char **arv);
-void free_list(list_path *head);
-
-
 #endif
